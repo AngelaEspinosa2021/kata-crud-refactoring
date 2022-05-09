82ff07c583d817e95e9c09eed9554ebc30e36165
AngelaEspinosa <titagena@gmail.com>
2022-05-08 19:28:57 -0500

    pendiente de pruebas de funcionamiento

--- back/src/main/java/co/com/sofka/crud/Model/GroupTodo.java
+++ back/src/main/java/co/com/sofka/crud/Model/GroupTodo.java
@@ -1,2 +1,49 @@
-package co.com.sofka.crud.Model;public class GroupTodo {
+package co.com.sofka.crud.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="group_todo")
+public class GroupTodo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id_groupTodo;
+
+    private String name;
+    private List<Todo> todoList;
+
+    public GroupTodo(){
+
+    }
+
+    public GroupTodo(String name, List<Todo> todoList) {
+        this.name = name;
+        this.todoList = todoList;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GroupTodo other = (GroupTodo) obj;
+        if (id_groupTodo == null) {
+            if (other.id_groupTodo != null)
+                return false;
+        } else if (!id_groupTodo.equals(other.id_groupTodo))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id_groupTodo == null) ? 0 : id_groupTodo.hashCode());
+        return result;
+    }
 }
--- back/src/main/java/co/com/sofka/crud/Model/Todo.java
+++ back/src/main/java/co/com/sofka/crud/Model/Todo.java
@@ -1,17 +1,32 @@
 package co.com.sofka.crud.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="todo")
 public class Todo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private boolean completed;
     private String groupListId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_groupTodo")
+    private GroupTodo groupTodo;
+
+    public Todo() {
+
+    }
+
+    public Todo(String name, boolean completed, String groupListId, GroupTodo groupTodo) {
+        this.name = name;
+        this.completed = completed;
+        this.groupListId = groupListId;
+        this.groupTodo = groupTodo;
+    }
 
     public String getGroupListId() {
         return groupListId;
@@ -43,5 +58,30 @@ public class Todo {
 
     public void setCompleted(boolean completed) {
         this.completed = completed;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Todo other = (Todo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
