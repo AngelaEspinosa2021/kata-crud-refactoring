571027b8abd882ad3a4c2f5776fa5ea58b6939d2
AngelaEspinosa <titagena@gmail.com>
2022-05-09 01:28:58 -0500

    pendiente creacion de Interfaz

--- back/src/main/java/co/com/sofka/crud/dto/GroupTodoDto.java
+++ back/src/main/java/co/com/sofka/crud/dto/GroupTodoDto.java
@@ -1,2 +1,52 @@
-package co.com.sofka.crud.dto;public class GroupTodoDto {
+package co.com.sofka.crud.dto;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+public class GroupTodoDto {
+    private Long id_groupTodo;
+
+    @NotBlank
+    private String name;
+    private List<TodoDto> todoList;
+
+    public GroupTodoDto(Long id_groupTodo, String name, List<TodoDto> todoList) {
+        this.id_groupTodo = id_groupTodo;
+        this.name = name;
+        this.todoList = todoList;
+    }
+
+    public GroupTodoDto(String name, List<TodoDto> todoList) {
+        this.name = name;
+        this.todoList = todoList;
+    }
+
+    public GroupTodoDto() {
+    }
+
+    public Long getId_groupTodo() {
+        return id_groupTodo;
+    }
+
+    public void setId_groupTodo(Long id_groupTodo) {
+        this.id_groupTodo = id_groupTodo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<TodoDto> getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(List<TodoDto> todoList) {
+        this.todoList = todoList;
+    }
+
+    
 }
